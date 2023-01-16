188f4cb23a4344f928e7422c059a9726c1d4d031
jlkiri <interalia.dev@gmail.com>
2023-01-16 20:40:28 +0900

    fix

--- attach.sh
+++ attach.sh
@@ -4,4 +4,6 @@ if_name="$1"
 
 bpftool prog load ipv6_filter.bpf.o /sys/fs/bpf/ipv6_filter
 program_id=$(bpftool prog list | grep filter_ipv6 | awk '{ print $1 }' | tr -d ':')
+
+# Change xdpgeneric to xdp for "real" XDP in the driver. XDP driver mode might not work on virtual cloud instances
 bpftool net attach xdpgeneric id "$program_id" dev "$if_name"
--- detach.sh
+++ detach.sh
@@ -2,6 +2,5 @@ set -eux
 
 if_name="$1"
 
-# Change xdpgeneric to xdp for "real" XDP in the driver. XDP driver mode might not work on virtual cloud instances
 bpftool net detach xdpgeneric dev "$if_name"
 rm -f /sys/fs/bpf/ipv6_filter
